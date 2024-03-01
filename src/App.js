b3899d9f8c8954e2fe3d19367a02c67e40462d9a
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-01 16:25:09 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Preferences from "./components/Preferences/Preferences";
+import Login from "./components/Login/Login";
 
 function App() {
   const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
 
   return (
     <div className="wrapper">
