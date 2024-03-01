3f9d8d1aa4eeda1ff94c1c8944e79df65b39475c
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-01 17:08:29 +0100

    update

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Preferences from "./components/Preferences/Preferences";
-import Login from "./components/Login/Login";
+import Dashboard from "../components/Dashboard/Dashboard";
+import Preferences from "../components/Preferences/Preferences";
+import Login from "../components/Login/Login";
 
 function setToken(userToken) {
   sessionStorage.setItem("token", JSON.stringify(userToken));
 }
-
-function getToken() {}
+function getToken() {
+  const tokenString = sessionStorage.getItem("token");
+  const userToken = JSON.parse(tokenString);
+  return userToken?.toke;
+}
 
 function App() {
   const token = getToken();
