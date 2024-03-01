63bafeefe493b562c4b0ca91f7fe507b8a56d517
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-01 16:22:38 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
-import Preferences from "./Preferences/Preferences";
+import Preferences from "./components/Preferences/Preferences";
 
 function App() {
+  const [token, setToken] = useState();
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
