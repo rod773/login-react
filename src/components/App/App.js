ad8dd8081760b80e9a8763b6c6a836c00508c982
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-01 17:25:56 +0100

    update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "components/Dashboard/Dashboard";
 import Preferences from "components/Preferences/Preferences";
--- src/components/App/useToken.js
+++ src/components/App/useToken.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 export default function useToken() {
   const getToken = () => {
     const tokenString = localStorage.getItem("token");
