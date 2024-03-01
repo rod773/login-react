7212abb9963da2616c3f32f4e85de908f6552a61
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-01 17:33:10 +0100

    local storage usetoken

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "components/Dashboard/Dashboard";
 import Preferences from "components/Preferences/Preferences";
