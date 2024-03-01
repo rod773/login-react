829107935e194f1217afa1fc7680a8983f7c750a
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-01 17:21:32 +0100

    useToken

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,18 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "components/Dashboard/Dashboard";
 import Preferences from "components/Preferences/Preferences";
 import Login from "components/Login/Login";
-
-function setToken(userToken) {
-  sessionStorage.setItem("token", JSON.stringify(userToken));
-}
-function getToken() {
-  const tokenString = sessionStorage.getItem("token");
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token;
-}
+import useToken from "./useToken";
 
 function App() {
-  const token = getToken();
+  const { token, setToken } = useToken();
 
   if (!token) {
     return <Login setToken={setToken} />;
