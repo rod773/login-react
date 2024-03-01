d4cbf4e82efe98537154a93c3a171a0b9787e14c
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-01 17:38:23 +0100

    update

--- src/components/App/useToken.js
+++ src/components/App/useToken.js
@@ -6,11 +6,14 @@ export default function useToken() {
     const userToken = JSON.parse(tokenString);
     return userToken?.token;
   };
+
   const [token, setToken] = useState(getToken());
+
   const saveToken = (userToken) => {
     localStorage.setItem("token", JSON.stringify(userToken));
     setToken(userToken.token);
   };
+
   return {
     setToken: saveToken,
     token,
